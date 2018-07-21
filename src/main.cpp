1491652dd85a6f99e242c1aa214fe4c41d28495d
Bob Lutz <rlutz@ggc.edu>
2018-07-21 14:53:05 -0400

    end of walkthrough, this is the baseline

--- src/main.cpp
+++ src/main.cpp
@@ -179,12 +179,12 @@ int main() {
 
   bool cold = true;        // cold start flag
   int lane = 1;            // start in lane 1 FROM Walkthrough
-  double ref_vel = 49.5;   // mph, move a reference velocity to target, FROM Walkthrough
+  double ref_vel = 0.0;   // mph, move a reference velocity to target, from walkthrough
 
 
   auto console = spdlog::stdout_color_mt("console");
-  console->info("Welcome to spdlog!");
-  console->error("Some error message with arg: {}", 1);
+  console->set_level(spdlog::level::debug);
+  console->info("Begin Path Planning");
 
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
@@ -213,7 +213,7 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&lane,&ref_vel,&cold,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&console,&lane,&ref_vel,&cold,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -232,26 +232,58 @@ int main() {
         
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
-        	// Main car's localization Data
-          	double car_x = j[1]["x"];
-          	double car_y = j[1]["y"];
-          	double car_s = j[1]["s"];
-          	double car_d = j[1]["d"];
-          	double car_yaw = j[1]["yaw"];
-          	double car_speed = j[1]["speed"];
 
-          	// Previous path data given to the Planner
-          	auto previous_path_x = j[1]["previous_path_x"];
-          	auto previous_path_y = j[1]["previous_path_y"];
-          	// Previous path's end s and d values 
-          	double end_path_s = j[1]["end_path_s"];
-          	double end_path_d = j[1]["end_path_d"];
+          // Main car's localization Data
+          double car_x = j[1]["x"];
+          double car_y = j[1]["y"];
+          double car_s = j[1]["s"];
+          double car_d = j[1]["d"];
+          double car_yaw = j[1]["yaw"];
+          double car_speed = j[1]["speed"];
 
-          	// Sensor Fusion Data, a list of all other cars on the same side of the road.
-          	auto sensor_fusion = j[1]["sensor_fusion"];
+          auto previous_path_x = j[1]["previous_path_x"];    // Previous path data given to the Planner
+          auto previous_path_y = j[1]["previous_path_y"];
+          double end_path_s = j[1]["end_path_s"];            // Previous path's end s and d values
+          double end_path_d = j[1]["end_path_d"];
 
-          	int prev_size = previous_path_x.size();
+          // Sensor Fusion Data, a list of all other cars on the same side of the road.
+          auto sensor_fusion = j[1]["sensor_fusion"];
+
+          int prev_size = previous_path_x.size();
+
+          if (prev_size > 0) car_s = end_path_s; // from walkthrough video
+          bool too_close = false;
+          double LANE_WIDTH = 4.0;
+          double HALF_LANE_WIDTH = LANE_WIDTH / 2.0;
+          for (int i = 0; i < sensor_fusion.size(); i++) { // find ref_v to use
+            float d = sensor_fusion[i][6]; // car is in my lane
+            if (d < (HALF_LANE_WIDTH + LANE_WIDTH * lane + HALF_LANE_WIDTH) &&
+                d > (HALF_LANE_WIDTH + LANE_WIDTH * lane - HALF_LANE_WIDTH)) {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx * vx + vy * vy);
+              double check_car_s = sensor_fusion[i][5];
+
+              check_car_s += ((double) prev_size * 0.02 * check_speed); // if using previous points can
+              // project s value outwards in time
+              if ((check_car_s > car_s) && ((check_car_s - car_s) < 30)) { // in meters
+                // set flag to slow and attempt lane change
+                too_close = true;
+                if (lane > 0) { // need state machine here 
+                  lane = 0;
+                }
+              }
+            }
+          }
+
+          if (too_close) {
+            ref_vel -= 0.224;
+            console->debug("decelerate to {}", ref_vel);
+          } else if (ref_vel < 49.5) {
+            ref_vel += 0.224;
+            console->debug("accelerate to {}", ref_vel);
+          }
+            // end of from walkthrough video
 
           	json msgJson;
 
@@ -266,9 +298,10 @@ int main() {
             double ref_y = car_y;
             double ref_yaw = deg2rad(car_yaw);
 
-            cout << "prev_size = " << prev_size << "\n";
+            console->trace("prev_size = {}", prev_size);
 
           if (cold) {
+            // hair is a fabricated vector, so that we have an initial direction
             vector<double> hair = getXY(car_s+0.1, car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
             ptsx.push_back(car_x);
             ptsx.push_back(hair[0]);
